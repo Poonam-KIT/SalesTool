0137736be3999bcb870c57682f1753dac78110a4
KITS <KITS@KITS-BD-01>
2015-03-19 16:14:05 +0530

    This is test commit to Sales Tool

---
+++ SalesTool/src/ca/demo/selestool/Main.java
@@ -0,0 +1,15 @@
+package ca.demo.selestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data=new SalesData();
+		dispalyGreeting();
+		data.display();
+	}
+private static void dispalyGreeting()
+{
+	System.out.println("Hello Happy Seles People");
+	System.out.println("This App Shows Seles data");
+}
+}
---
+++ SalesTool/src/ca/demo/selestool/SalesData.java
@@ -0,0 +1,10 @@
+package ca.demo.selestool;
+
+public class SalesData {
+	int data[]={0,4,42};
+	public void display()
+	{
+		System.out.println("1000  Imlpement data display");
+	}
+
+}
